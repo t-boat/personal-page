45b12553de4e43fc613b4bc9cefc1d7afd1fc519
Theophilus Boateng <tboateng2011@gmail.com>
2024-08-25 21:37:27 +0000

    added my cv

--- src/section-components/skillTile.jsx
+++ src/section-components/skillTile.jsx
@@ -16,7 +16,7 @@ const SkillTile = () => {
 
             <nav className="absolute top-0 w-full flex justify-between items-center py-[2.5rem] px-[5rem]">
 
-                <a href="" download className='flex gap-2 w-max rounded px-3 py-[0.35rem] text-[#04131F] font-bold border-2 border-[#04131F]' style={{fontFamily: 'Allerta Stencil'}}> <Download /> Resume </a>
+                <a href="/src/assets/files/Theophilus-Boateng-CV.pdf" download className='flex gap-2 w-max rounded px-3 py-[0.35rem] text-[#04131F] font-bold border-2 border-[#04131F]' style={{fontFamily: 'Allerta Stencil'}}> <Download /> Resume </a>
 
                 <div className="w-[40%] flex justify-between font-semibold text-[#04131F]" style={{fontFamily: 'Allerta Stencil'}}>
 
