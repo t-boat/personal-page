fd9c7f9018b6a192f80bdb055890a55ac6276f23
Theophilus Boateng <tboateng2011@gmail.com>
2024-10-06 17:41:42 +0000

    minor changes

--- src/pages/works.jsx
+++ src/pages/works.jsx
@@ -1,14 +1,50 @@
+import { useMediaQuery } from "react-responsive";
+
 
 const Works = () => {
+
+    //Define the breakpoints
+    const isTouch = useMediaQuery({ maxWidth: 850.9 });
+    const defaultView = useMediaQuery({ minWidth: 851 });
+
+
     return (
-        <div className="under-construction w-full h-screen">
 
-            <div className="text-[40pt] neons">
-                Page under construction <br />
-                <p className="text-[20pt]">kindly check back later</p>
-            </div>
+        <>
 
-        </div>
+            {defaultView &&
+
+                <div className="under-construction w-full h-screen">
+
+                    <div className="text-[5em] neons flex flex-col">
+
+                        <p>Page under construction</p>
+
+                        <p className="text-[3rem]">kindly check back later</p>
+
+                    </div>
+
+                </div>
+            }
+
+
+            {isTouch &&
+
+                <div className="under-construction w-full h-screen">
+
+                    <div className="text-[3.5em] neons flex flex-col">
+
+                        <p>Page under construction</p>
+
+                        <p className="text-[1.8rem]">kindly check back later</p>
+
+                    </div>
+
+                </div>
+            }
+
+        </>
+
     );
 };
 
